547fd88b6e03f3318ab8a77820aa3581a146398e
Defa_Danuarta <kdkdefa@gmail.com>
2025-11-18 10:03:35 +0700

    debugging the ContactCard email option

--- components/ContactCard.js
+++ components/ContactCard.js
@@ -10,7 +10,11 @@ export default function ContactCard() {
                 <p className="mt-2 text-sm font-extralight text-neutral-400 md:text-base">
                     Feel free to contact me to start collaborate :D
                 </p>
-                <a href="mailto:defa.danuarta@email.com?subject=Lets%20Collaborate!&body=Hi%20Defa,%0D%0A%0D%0AI&apos;m%20interested%20in%20working%20with%20you%20on%20a%20project.%0D%0A%0D%0ARegards,%0D%0A[Your%20Name]">
+                <a
+                    href={`mailto:defa.danuarta@email.com 
+                        ?subject=Let's%20Collaborate!
+                        &body=Hi%20Defa,%0D%0A%0D%0AI'm%20interested%20in%20working%20with%20you%20on%20a%20project.%0D%0A%0D%0ARegards,%0D%0A[Your%20Name]`}
+                >
                     <button className="mt-4 px-4 py-2 border text-[#C68F6B] border-[#C68F6B] rounded-full hover:bg-[#C68F6B] hover:text-black transition-all text-sm font-extralight">
                         Mail Me <i className="ms-2 fa-solid fa-envelope"></i>
                     </button>
